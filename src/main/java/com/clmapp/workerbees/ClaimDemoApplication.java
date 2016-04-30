3c156e94af56321f333b00b1de5c23820f8d5c6f
Sindhu Emilda <emildasindhu@gmail.com>
2016-04-29 23:24:00 -0400

    Added file upload

--- src/main/java/com/clmapp/workerbees/ClaimDemoApplication.java
+++ src/main/java/com/clmapp/workerbees/ClaimDemoApplication.java
@@ -1,7 +1,13 @@
 package com.clmapp.workerbees;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClaimDemoApplication {
 
+	public static String ROOT = "upload";
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClaimDemoApplication.class, args);
 
 	}
 	
-	@RequestMapping("/home")
+	@RequestMapping("/myhome")
 	public String home() {
 		return "Hello Worker Bees Docker";
 	}
+	
+	@Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdir();
+            log.debug("Created dir to upload files into");
+        };
+    }
 }
